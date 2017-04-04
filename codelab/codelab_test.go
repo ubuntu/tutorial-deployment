4dc8daa3636011e74a36445025871239bd7f07c3
Didier Roche <didrocks@ubuntu.com>
2017-04-04 16:41:35 +0200

    Add a testcase with a simple image in md file directory

--- codelab/codelab_test.go
+++ codelab/codelab_test.go
@@ -31,6 +31,7 @@ func TestGenerateCodelabs(t *testing.T) {
 		{"testdata/codelabsrc/markdown-no-image.md", false, nil, nil, false},
 		{"testdata/codelabsrc/markdown-no-image.md", true, []string{"testdata/codelabsrc/markdown-no-image.md"}, nil, false},
 		{"testdata/codelabsrc/markdown-invalid-generated-html.md", false, nil, []string{"example-snap-tutorial/index.inc"}, false},
+		{"testdata/codelabsrc/markdown-with-images-simple.md", false, nil, nil, false},
 	}
 	for _, tc := range testCases {
 		t.Run(fmt.Sprintf("generate %s", tc.src), func(t *testing.T) {
