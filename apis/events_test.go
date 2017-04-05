6975e82ebea002a1649cafb7d9b64ab1ff6f5d77
Didier Roche <didrocks@ubuntu.com>
2017-04-05 13:10:22 +0200

    Add test to save images at the correct place

--- apis/events_test.go
+++ apis/events_test.go
@@ -3,6 +3,7 @@ package apis
 import (
 	"fmt"
 	"io/ioutil"
+	"path"
 	"reflect"
 	"testing"
 
@@ -50,6 +51,53 @@ func TestNewEvents(t *testing.T) {
 
 			if !reflect.DeepEqual(*e, tc.wantEvents) {
 				t.Errorf("Generated events: got %+v; want %+v", *e, tc.wantEvents)
+			}
+		})
+	}
+}
+
+func TestSaveImages(t *testing.T) {
+	testCases := []struct {
+		eventsDir string
+		eventsObj Events
+
+		wantEvents Events
+		wantErr    bool
+	}{
+		{"testdata/events/valid",
+			Events{"event-1": event{Name: "Event 1", Logo: "img/event1.jpg", Description: "This workshop is taking place at Event 1."},
+				"event-2": event{Name: "Event 2", Logo: "event2.jpg", Description: "This workshop is taking place at Event 2."},
+			},
+			Events{"event-1": event{Name: "Event 1", Logo: "assets/event1.jpg", Description: "This workshop is taking place at Event 1."},
+				"event-2": event{Name: "Event 2", Logo: "assets/event2.jpg", Description: "This workshop is taking place at Event 2."},
+			},
+			false},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("save events: %+v", tc.eventsDir), func(t *testing.T) {
+			// Setup/Teardown
+			out, teardown := tempDir(t)
+			defer teardown()
+			p, teardown := paths.MockPath()
+			defer teardown()
+			p.MetaData = tc.eventsDir
+			p.API = out
+
+			// Test
+			err := tc.eventsObj.SaveImages()
+
+			if (err != nil) != tc.wantErr {
+				t.Errorf("SaveImages() error = %v, wantErr %v", err, tc.wantErr)
+			}
+
+			if !reflect.DeepEqual(tc.eventsObj, tc.wantEvents) {
+				t.Errorf("Image paths not changed in event: got %+v; want %+v", tc.eventsObj, tc.wantEvents)
+			}
+			for _, e := range tc.wantEvents {
+				imgP := path.Join(p.API, e.Logo)
+				if _, err := os.Stat(imgP); os.IsNotExist(err) {
+					t.Errorf("%s doesn't exist when we wanted it", imgP)
+				}
 			}
 		})
 	}
