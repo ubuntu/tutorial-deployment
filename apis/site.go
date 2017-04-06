ecea00899194ecc58103e96209a567e6d7063a87
Didier Roche <didrocks@ubuntu.com>
2017-04-06 17:57:57 +0200

    Misc cleanup

--- apis/site.go
+++ apis/site.go
@@ -45,20 +45,3 @@ func Save(dat []byte) error {
 	p := paths.New()
 	return ioutil.WriteFile(path.Join(p.API, apiFileName), dat, 0644)
 }
-
-/*
-
-// Codelab is the stringify representation of a codelab needed by the site
-type Codelab struct {
-	Source     string             `json:"source"`
-	Title      string             `json:"title"`
-	Summary    string             `json:"summary"`
-	Category   []string           `json:"category"`
-	Difficulty int                `json:"difficulty"`
-	Duration   int                `json:"duration"`
-	Tags       []string           `json:"tags"`
-	Updated    *types.ContextTime `json:"updated"`
-	URL        string             `json:"url"`
-}
-
-*/
